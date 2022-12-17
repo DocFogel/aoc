3cfe0e0110787f6016d13767d4bbd10b36206013
Jonas Fogelberg <doc.fogel@gmail.com>
2022-12-17 11:27:25 +0100

    days 7 and 8

---
+++ day7.js
@@ -0,0 +1,80 @@
+class Folder {
+    constructor(name) {
+        this.name = name;
+        this.folders = new Array();
+        this.files = new Array();
+    }
+
+    addFolder(name) {
+        const newFolder = new Folder(name)
+        this.folders.push(newFolder);
+        return newFolder;
+    }
+
+    addFile(fileName, fileSize) {
+        this.files.push({name: fileName, size: fileSize});
+    }
+
+    getSize() {
+        return this.files.reduce((t, f) => t + f.size, 0);
+    }
+}
+
+class Context {
+    constructor() {
+        this.rootNode = null;
+        this.currentFolder = null;
+        this.parents = new Array();
+        this.directorySizes = new Array();
+
+    }
+
+    onLine(line) {
+        if (line.startsWith('$ cd ..')) {
+            this.currentFolder = this.parents.pop();
+        } else if (line.startsWith('$ cd /')) {
+            this.rootNode = new Folder('root');
+            this.currentFolder = this.rootNode;
+            this.parents.push(this.rootNode);
+        } else if (line.startsWith('$ cd')) {
+            const newFolder = this.currentFolder.addFolder(line.substring(5));
+            this.parents.push(this.currentFolder);
+            this.currentFolder = newFolder;
+        } else if (/^\d+/.test(line)) {
+            const [ sizeStr, name ] = line.split(' ');
+            this.currentFolder.addFile(name, parseInt(sizeStr));
+        }
+    }
+
+    onClose() {
+        const rootsize = this.walkFolders(this.rootNode);
+        const reclaimSize = rootsize - (70000000 - 30000000);
+        const smallFolderTotal = this.directorySizes
+            .filter(s => s < 100000)
+            .reduce((t, s) => t + s);
+        const deleteCandidates = this.directorySizes
+            .filter(s => s > reclaimSize);
+        const smallestToDelete = Math.min(...deleteCandidates);
+
+        console.log('Sum om small folders:', smallFolderTotal);
+        console.log('Smallest to delete: ', smallestToDelete);
+    }
+
+    walkFolders(folder, basepath = '') {
+        var totalSize = folder.getSize();
+        const folderPath = basepath + '/' + folder.name;
+        for (const f of folder.folders) {
+            const childSize = this.walkFolders(f, folderPath);
+            this.directorySizes.push(childSize);
+            totalSize += childSize;
+        }
+        return totalSize;
+    }
+}
+
+const strm = require('node:readline').createInterface({
+    input: require('node:process').stdin
+});
+const readLines = require('./readstream').readContent;
+readLines(strm, 
+    new Context());
---
+++ day8.js
@@ -0,0 +1,81 @@
+const { timeStamp } = require('node:console');
+
+class Context {
+    constructor() {
+        this.grove = new Array();
+        this.width = 0;
+        this.depth = 0;
+    }
+
+    onLine(line) {
+        this.width = line.length;
+        this.depth++;
+        this.grove.push(...line.split('').map(v => ({ 
+            height: parseInt(v), 
+            visibility: 0,
+            score: { north: 0, south: 0, east:0, west:0 }
+        })));
+    }
+
+    onClose() {
+        this.grove.forEach((tree, index) => {
+            tree.score.east = this.viewEast(index);
+            if (tree.score.east <= 0) {
+                tree.visibility += 1;
+            }
+            tree.score.north = this.viewNorth(index);
+            if (tree.score.north <= 0) {
+                tree.visibility += 2;
+            }
+            tree.score.west = this.viewWest(index);
+            if (tree.score.west <= 0) {
+                tree.visibility += 4;
+            }
+            tree.score.south = this.viewSouth(index);
+            if (tree.score.south <= 0) {
+                tree.visibility += 8;
+            }
+        })
+        console.log(this.grove.reduce((tot, tree)=>tot += Math.sign(tree.visibility), 0), "trees are visible")
+        console.log(Math.max(...this.grove.map(t => Math.abs(t.score.east * t.score.west * t.score.north * t.score.south))));
+    }
+     
+    viewWest(index) {
+        let row = Math.trunc(index / this.width);
+        return this.viewScore(index, -1, row * this.width, (row + 1) * this.width - 1);
+    }
+
+    viewEast(index) {
+        let row = Math.trunc(index / this.width);
+        return this.viewScore(index, 1, row * this.width, (row + 1) * this.width - 1);
+    }
+
+    viewSouth(index) {
+        return this.viewScore(index, this.width, 0, this.grove.length - 1);
+    }
+
+    viewNorth(index) {
+        return this.viewScore(index, -this.width, 0, this.grove.length - 1);
+    }
+
+    viewScore(index, step, lowerBounds, upperBounds) {
+        let score = 0;
+        let viewHeight = this.grove.at(index).height;
+        index += step;
+        while (index >= lowerBounds && index <= upperBounds) {
+            score++;
+            if (this.grove.at(index).height >= viewHeight) {
+                return score;
+            }
+            index += step;
+        }
+        return -score;
+    }
+}
+
+const strm = require('node:readline').createInterface({
+    input: require('node:process').stdin
+});
+const readLines = require('./readstream').readContent;
+readLines(strm, 
+    new Context());
