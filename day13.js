6938adc60e8abb74adcad86c44cd2b1f3037af27
Jonas Fogelberg <jonas.fogelberg@sectra.com>
2023-01-04 13:39:43 +0100

    day13 part 2

--- day13.js
+++ day13.js
@@ -4,27 +4,34 @@ class Context {
         this.right = null;
         this.currentPairIndex = 1;
         this.correctPacketSum = 0;
+        this.packets = new Array();
     }
 
     onLine(line) {
         if (line.trim().length === 0) {
-            this.correctPacketSum += this.compareLists(this.left, this.right) < 0 ? this.currentPairIndex : 0;
-            this.currentPairIndex++;
-            this.left = this.right = null;
+          // skip this
+          return;
+        }
+        if (!this.left) {
+          this.left = JSON.parse(line);
+          this.packets.push(this.left);
         } else {
-            if (!this.left) {
-                this.left = JSON.parse(line);
-            } else {
-                this.right = JSON.parse(line);
-            }
+          this.right = JSON.parse(line);
+          this.packets.push(this.right);
+          this.correctPacketSum += this.compareLists(this.left, this.right) < 0 ? this.currentPairIndex : 0;
+          this.currentPairIndex++;
+          this.left = this.right = null;
         }
     }
 
     onClose() {
-        if (this.left && this.right) {
-            this.correctPacketSum += this.compareLists(this.left, this.right) < 0 ? this.currentPairIndex : 0;
-        }
         console.log('Sum of correct packets:', this.correctPacketSum);
+
+        let dividers = [[[2]], [[6]]];
+        dividers.forEach(d => d.divider = true);
+        this.packets.push(...dividers);        
+        console.log(this.packets.length, this.packets.sort((a,b)=>this.compareLists(a,b)));
+        console.log('decoder key:', this.packets.reduce((t, p, i) => t * (p.divider ? i + 1 : 1), 1));
     }
 
     compareLists(left, right) {
