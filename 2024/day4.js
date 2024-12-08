5413a998a73f86b71a58c140334efebefcba85e2
Jonas Fogelberg <jonas.fogelberg@sectra.com>
2024-12-08 19:24:23 +0100

    Day 4 part 2 2024

--- 2024/day4.js
+++ 2024/day4.js
@@ -2,12 +2,12 @@
 class Context {
     stepMap = new Map([
         ['n',  { x: 0, y: -1 }],
-        ['ne', { x: 1, y: -1 }],
         ['e',  { x: 1, y: 0 }],
-        ['se', { x: 1, y: 1 }],
         ['s',  { x: 0, y: 1 }],
-        ['sw', { x: -1, y: 1 }],
         ['w',  { x: -1, y: 0 }],
+        ['ne', { x: 1, y: -1 }],
+        ['se', { x: 1, y: 1 }],
+        ['sw', { x: -1, y: 1 }],
         ['nw', { x: -1, y: -1 }]
     ]);
 
@@ -30,14 +30,26 @@ class Context {
         // Part 1
         // find possible starting points and check what directions do NOT step out of bounds
         // this will be a list of starting points and directions
-        let candidates = this.puzzle.map((line, row) => this.findXs(line).map(x => this.allDirections().map(dir => ({x: x, y: row, direction: dir})))).flat(2);
+        let candidates = this.puzzle
+            .map((line, row) => this.findStarts("X", line).map(x => this.allDirections().map(dir => ({x: x, y: row, direction: dir}))))
+            .flat(2);
 
-        const matches = candidates.filter(this.isXMAS, this);
+        let matches = candidates.filter(this.isXMAS, this);
         console.log(`Total part 1 matches: ${matches.length}`);
+
+        // Part 2
+        candidates = this.puzzle
+            .map((line, row) => this.findStarts("A", line).map(x => ({x: x, y: row, direction: this.allDirections().filter(dir => dir.length === 2) })))
+            .flat(1)
+            .filter(c => c.x >= 1 && c.x <= this.puzzleWidth - 2 && c.y >= 1 && c.y <= this.puzzle.length - 2);
+
+        matches = candidates.filter(this.isX_MAS, this);
+        console.log(`Total part 2 matches: ${matches.length}`)
     }
 
-    findXs(line) {
-        return Array.from(line.matchAll(/X/g)).map(m => m.index);
+    findStarts(c, line) {
+        const pattern = new RegExp(c, "g");
+        return Array.from(line.matchAll(pattern)).map(m => m.index);
     }
 
     allDirections() {
@@ -57,6 +69,15 @@ class Context {
                 && y < this.puzzle.length
                 && (this.puzzle[y][x] === expected)
         }, true);
+    }
+
+    isX_MAS(origin) {
+        let mas = origin.direction.filter(dir => {
+            const step = this.stepMap.get(dir);
+            return this.puzzle[origin.y - step.y][origin.x - step.x] === "M" 
+                    && this.puzzle[origin.y + step.y][origin.x + step.x] === "S";
+        })
+        return mas.length >= 2;
     }
 }
 
