fcbc9604778dec27c4e8c079c4e6d6ce97132d55
Jonas Fogelberg <fogelberg.jonas@gmail.com>
2024-12-23 22:02:24 +0100

    Day 6 part 2 2024 wip

--- 2024/day6.mjs
+++ 2024/day6.mjs
@@ -18,44 +18,61 @@ class Stretch {
         return Math.abs(this.end.y - this.start.y);
     }
 
-    isCrossing(stretch) {
+    crossing(stretch) {
         if (this.direction % 2 === stretch.direction % 2) {
             // both stretches are horizontal or vertical, they can't cross
-            return false;
+            return null;
         }
         if (this.start.x === stretch.end.x && this.start.y === stretch.end.y) {
             // stretch ending on this's start does not cross
-            return false;
+            return null;
         }
-        if (this.direction % 2 === 0) {
-            // this stretch is horizontal
-            return Math.min(this.start.x, this.end.x) <= stretch.start.x && Math.max(this.start.x, this.end.x) >= stretch.start.x
-                && Math.min(stretch.start.y, stretch.end.y) <= this.start.y && Math.max(stretch.start.y, stretch.end.y) >= this.start.y;
+        if (this.direction % 2 === 0
+            && Math.min(this.start.x, this.end.x) <= stretch.start.x && Math.max(this.start.x, this.end.x) >= stretch.start.x
+            && Math.min(stretch.start.y, stretch.end.y) <= this.start.y && Math.max(stretch.start.y, stretch.end.y) >= this.start.y) {
+            // this stretch is horizontal and crossing
+            return { x: stretch.start.x, y: this.start.y };
+        } else if (this.direction % 2 !== 0
+            && Math.min(stretch.start.x, stretch.end.x) <= this.start.x && Math.max(stretch.start.x, stretch.end.x) >= this.start.x
+            && Math.min(this.start.y, this.end.y) <= stretch.start.y && Math.max(this.start.y, this.end.y) >= stretch.start.y) {
+            // this stretch is vertical and crossing
+            return { x: this.start.x, y: stretch.start.y };
         }
-        // this stretch is vertical
-        return Math.min(stretch.start.x, stretch.end.x) <= this.start.x && Math.max(stretch.start.x, stretch.end.x) >= this.start.x
-            && Math.min(this.start.y, this.end.y) <= stretch.start.y && Math.max(this.start.y, this.end.y) >= stretch.start.y;
+
+        // stretches are not crossing
+        return null;
     }
 }
 
 class Guard {
     constructor(room) {
         this.room = room;
-        this.position = room.start;
+        this.position = { ...room.start };
         this.totalCrosses = 0;
         this.walkedStretches = [];
+        this.fullStretches = [];
+        this.vulnerablePoints = [];
     }
 
     nextStretch() {
         if (this.exited) {
             return null;
         }
-        let obstacle = this.room.findObstacle(this.position);
+        let otherWay = (this.position + 2) % 2;
 
-        const destination = this.getNextTo(obstacle, this.position.direction);
+        const destination = this.getNextTo(this.room.findObstacle(this.position), this.position.direction);
         let thisStretch = new Stretch(this.position, destination);
-        this.totalCrosses += this.walkedStretches.filter(walked => thisStretch.isCrossing(walked)).length;
+        this.totalCrosses += this.walkedStretches.filter(walked => thisStretch.crossing(walked) != null).length;
         this.walkedStretches.push(thisStretch);
+        let vulnerableCrossings = this.fullStretches
+            .map(f_stretch => f_stretch.direction === this.turnRight(thisStretch.direction) ? thisStretch.crossing(f_stretch) : null)
+            .filter(p => p !== null);
+        this.vulnerablePoints.push(...vulnerableCrossings);
+
+        this.fullStretches.push(
+            new Stretch(
+                this.getNextTo(this.room.findObstacle({ ...this.position, direction: otherWay}), otherWay),
+                destination));
         this.position = destination;
         this.position.direction = this.turnRight(this.position.direction);
         return thisStretch;
